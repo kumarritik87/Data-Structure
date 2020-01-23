98e6abb460521964f6c397e88accab628192b94c
Ritik Kumar <ritik.kumar_cs18@gla.ac.in>
2020-01-24 00:12:10 +0530

    Fibonacci Series proram using recursion in java

---
+++ FiibonacciSeries.java
@@ -0,0 +1,26 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class FiibonacciSeries {
+    static int fibo(int n) {
+        if (n < 1) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return (n - 1);
+        } else {
+            return (fibo(n - 1) + fibo(n - 2));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        FiibonacciSeries fb = new FiibonacciSeries();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibo(i) + " ");
+        }
+    }
+}
